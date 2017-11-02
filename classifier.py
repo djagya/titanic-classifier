22460b8b6c31f6c409b9b0bf0b27aa4023f2599a
djagya <danil.kabluk@gmail.com>
2017-11-02 15:26:29 +0100

    try to load data

--- classifier.py
+++ classifier.py
@@ -2,10 +2,12 @@ import pandas as pd
 import numpy as np
 import math
 
+
 def train():
     print("train model")
 
 
 def load_data(filename):
-    print("load data")
-    train_data = pd.read_csv('data/train.csv')
+    data = pd.read_csv(filename)
+
+    return data
